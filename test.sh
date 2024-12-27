4a6417038912ec2fca7d6fa641a3076b7937a293
Manojkumbi <Manojkumbi2803@gmail.com>
2024-12-27 22:41:27 +0530

    changes detected

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Purpose:testing quotes
+#Version:0
+#Created date:Fri Dec 27 08:00:09 PM IST 2024
+#Modified date:`date`
+#Author:KUMARASWAMY S
+#START#
+
+var=123
+
+echo "testing this script $var"
+echo 'testing $var'
+echo `date`
+
+#END#
---
+++ var.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Purpose:
+#Version:
+#Created date:Fri Dec 27 10:05:14 PM IST 2024
+#Modified date:
+#Author:KUMARASWAMY S
+#START#
+
+echo "$*"
+echo "$#"
+echo "$1"
+echo "$2"
+echo "$0"
+echo "$@"
+echo "$?"
+echo "$$"
+echo "$!"
+
+
+#END#
